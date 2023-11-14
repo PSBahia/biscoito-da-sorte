2fed6622f02a3c12f8a4e245fabfe9d024c2dba9
PSBahia <paulo.rpba@gmail.com>
2023-11-14 06:27:12 -0300

    random pelo tamanho do array

--- main.js
+++ main.js
@@ -8,19 +8,23 @@ const btnRest = document.querySelector("#btnRest")
 
 function handleTryClick(event){
     event.preventDefault()
-    let randomNumber = Math.round(Math.random() * 4)
+    
     const arrayLuck = [
         '"Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu."',
         '"Nossos fracassos, às vezes, são mais frutíferos do que os êxitos"',
         '"Tente de novo. Fracasse de novo. Mas fracasse melhor"',
         '"É costume de um tolo, quando erra, queixar-se dos outros. É costume de um sábio queixar-se de si mesmo"',
-        '"Mesmo que já tenhas feito uma longa caminhada, há sempre um novo caminho a fazer"'
+        '"Mesmo que já tenhas feito uma longa caminhada, há sempre um novo caminho a fazer"',
+        '"Quando chover, busque pelo arco-íris. Quando escurecer, busque pelas estrelas."'
     ]
 
-    
+    let arraySize = arrayLuck.length - 1
+    let randomNumber = Math.round(Math.random() * arraySize)
+
     toggleScreen()
     document.querySelector(".screen2 .card-luck").innerText =  arrayLuck[randomNumber]
     console.log(randomNumber)
+    console.log(arrayLuck.length)
 }
 
 function handleResetClick(){
